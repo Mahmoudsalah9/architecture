880f8fe59228a159ddd8dc40d04463591cfa663f
Yasser Amr Eldahrawy <96025586+YasserEldahrawy@users.noreply.github.com>
2024-04-24 19:06:07 +0200

    Update instruction_pointer.vhd

--- instruction_pointer.vhd
+++ instruction_pointer.vhd
@@ -1,33 +1,31 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
- -- make sure that the we is always zero 
- LIBRARY IEEE;
- USE IEEE.STD_LOGIC_1164.ALL;
- USE IEEE.numeric_std.all;
- 
- ENTITY instruction_pointer IS
-	 PORT(
-		 clk : IN std_logic;
-		 we  : IN std_logic;
-		 address : IN  std_logic_vector(11 DOWNTO 0);
-		 datain  : IN  std_logic_vector(15 DOWNTO 0);
-		 dataout : OUT std_logic_vector(15 DOWNTO 0));
- END ENTITY instruction_pointer;
-	-- make sure that the we is always zero 
- 
- ARCHITECTURE syncrama OF instruction_pointer IS
- 
-	 TYPE ram_type IS ARRAY(0 TO 4095) OF std_logic_vector(15 DOWNTO 0);
-	 SIGNAL ram : ram_type ;
-	 
-	 BEGIN
-		 PROCESS(clk) IS
-			 BEGIN
-				 IF rising_edge(clk) THEN  
-					 IF we = '1' THEN
-						 ram(to_integer(unsigned(address))) <= datain;
-					 END IF;
-				 END IF;
-		 END PROCESS;
-		 dataout <= ram(to_integer(unsigned(address)));
- END syncrama;
- 
+ENTITY Instruction_Memory IS
+	PORT (
+		CLK : IN STD_LOGIC;
+		we : IN STD_LOGIC;
+		address : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
+		datain : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+		dataout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
+END ENTITY Instruction_Memory;
+
+-- make sure that the we is always zero 
+
+ARCHITECTURE Instruction_Memory_Design OF Instruction_Memory IS
+
+	TYPE ram_type IS ARRAY(0 TO 4095) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
+	SIGNAL ram : ram_type;
+
+BEGIN
+	PROCESS (CLK) IS
+	BEGIN
+		IF rising_edge(CLK) THEN
+			IF we = '1' THEN
+				ram(to_integer(unsigned(address))) <= datain;
+			END IF;
+		END IF;
+	END PROCESS;
+	dataout <= ram(to_integer(unsigned(address)));
+END Instruction_Memory_Design;
