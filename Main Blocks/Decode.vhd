dd9c094559b166530ba86015c8ba0746a8487f80
Yasser Amr Eldahrawy <96025586+YasserEldahrawy@users.noreply.github.com>
2024-04-25 01:02:54 +0200

    43
    dfsdf

---
+++ Main Blocks/Decode.vhd
@@ -0,0 +1,22 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY Decode IS
+    PORT (
+
+        Clk : IN STD_LOGIC;
+        Rst : IN STD_LOGIC;
+        Data_After : IN STD_LOGIC;
+
+    );
+END ENTITY;
+
+ARCHITECTURE Decode_Design OF Decode IS
+
+
+
+
+
+
+BEGIN
+END;
---
+++ Main Blocks/Execute.vhd
@@ -0,0 +1,22 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY Execute IS
+    PORT (
+
+        Clk : IN STD_LOGIC;
+        Rst : IN STD_LOGIC;
+        
+
+    );
+END ENTITY;
+
+ARCHITECTURE Execute_Design OF Execute IS
+
+
+
+
+
+
+BEGIN
+END;
