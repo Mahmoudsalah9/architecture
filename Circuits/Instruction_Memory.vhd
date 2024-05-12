b8c0796f7c455d76acf2c67bb55a040473cc9156
Mahmoudsalah9 <134644754+Mahmoudsalah9@users.noreply.github.com>
2024-05-13 00:25:44 +0300

    UPDATED THE INSTRUCTION MEMORY TO MATCH THE DESIGN

--- Circuits/Instruction_Memory.vhd
+++ Circuits/Instruction_Memory.vhd
@@ -3,23 +3,27 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.ALL;
 
 ENTITY Instruction_Memory IS
-	PORT (
-		CLK : IN STD_LOGIC;
-		address : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
-		dataout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
-END ENTITY;
+    PORT (
+        CLK : IN STD_LOGIC;
+        address : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
+        data   : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        M10   : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        M32   : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        Instruction  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+    );
+END ENTITY Instruction_Memory;
 
 ARCHITECTURE Instruction_Memory_Design OF Instruction_Memory IS
-
-	TYPE ram_type IS ARRAY(0 TO 4095) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
-	SIGNAL ram : ram_type;
-
+    TYPE ram_type IS ARRAY(0 TO 4095) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL ram : ram_type;
 BEGIN
-	PROCESS (CLK) IS
-	BEGIN
-		IF rising_edge(CLK) THEN
-			dataout <= ram(to_integer(unsigned(address)));
-		END IF;
-	END PROCESS;
-
-END ARCHITECTURE;
+    PROCESS (CLK) IS
+    BEGIN
+        IF rising_edge(CLK) THEN
+            Instruction <= ram(to_integer(unsigned(address)));
+            data <= ram(to_integer(unsigned(address))+1);
+            M10  <= ram(1) & ram(0);
+            M32  <= ram(3) & ram(2);
+        END IF;
+    END PROCESS;
+END ARCHITECTURE Instruction_Memory_Design;
