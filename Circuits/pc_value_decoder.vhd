b6f74a2af492ade1f21c3b433c9797e18777ca94
Yasser Amr Eldahrawy <96025586+YasserEldahrawy@users.noreply.github.com>
2024-05-14 00:38:30 +0300

    Update pc_value_decoder.vhd

--- Circuits/pc_value_decoder.vhd
+++ Circuits/pc_value_decoder.vhd
@@ -1,23 +1,26 @@
-library ieee;
-use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
-entity pc_value_decoder is
-  port (
-    jmp_destination,result_memory,M10,M32,jmp_zero_desination : in std_logic_vector(31 to 0);
-    BRANCH_ZERO,zero_flag,update_pc_rti,update_pc_int,reset,ret_enable,jump_ebnable:in std_logic; 
-    pc_decoder_enable : out std_logic;
-    adress_pc_decoder_value : out std_logic_vector(11 downto 0)
+ENTITY pc_value_decoder IS
+  PORT (
+    jmp_destination, result_memory, M10, M32, jmp_zero_desination : IN STD_LOGIC_VECTOR(31 TO 0);
+    BRANCH_ZERO, zero_flag, update_pc_rti, update_pc_int, reset, ret_enable, jump_ebnable : IN STD_LOGIC;
+    pc_decoder_enable : OUT STD_LOGIC;
+    adress_pc_decoder_value : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)
   );
-end entity pc_value_decoder;
+END ENTITY pc_value_decoder;
 
-architecture rtl of pc_value_decoder is
-begin
-  pc_decoder_enable<='1'when ((BRANCH_ZERO='1'and zero_flag='1') or update_pc_rti='1'  or update_pc_int='1' or reset='1' or ret_enable='1' or jump_ebnable='1') else '0';
-  adress_pc_decoder_value<=jmp_destination when jump_ebnable='1' else
-                           result_memory when ret_enable='1' else
-                           M10 when update_pc_rti='1' else
-                           M32 when update_pc_int='1' else
-                           jmp_zero_desination when BRANCH_ZERO='1' and zero_flag='1' else
-                           (others=>'0');
-  
-end architecture rtl;
+ARCHITECTURE rtl OF pc_value_decoder IS
+BEGIN
+  pc_decoder_enable <= '1'WHEN ((BRANCH_ZERO = '1'AND zero_flag = '1') OR update_pc_rti = '1' OR update_pc_int = '1' OR reset = '1' OR ret_enable = '1' OR jump_ebnable = '1') ELSE
+    '0';
+
+  adress_pc_decoder_value <= jmp_destination WHEN jump_ebnable = '1' ELSE
+    result_memory WHEN ret_enable = '1' ELSE
+    result_memory WHEN update_pc_rti = '1' ELSE
+    M10 WHEN reset = '1' ELSE
+    M32 WHEN update_pc_int = '1' ELSE
+    jmp_zero_desination WHEN BRANCH_ZERO = '1' AND zero_flag = '1' ELSE
+    (OTHERS => '0');
+
+END ARCHITECTURE rtl;
