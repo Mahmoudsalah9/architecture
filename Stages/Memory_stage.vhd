86d47d7dba1c131658e29c74acb9bcc71876564c
Abdelrahman Ashraf <abdelrahman.a.3000@gmail.com>
2024-05-18 02:08:24 +0300

    nbnv

--- Stages/Memory_stage.vhd
+++ Stages/Memory_stage.vhd
@@ -11,7 +11,8 @@ ENTITY Memory_Stage IS
         Write_Enable_in, Mem_Write_enable, Mem_Write_INT_FSM : IN STD_LOGIC;
         Ret_Enable_In : IN STD_LOGIC;
         Write_Data_Selector, Mux_Select_INT_FSM : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        CCR_Out, PC_Address_INT_OUT, Read_port2_data_in, PC_Value : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        CCR_Out : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        PC_Address_INT_OUT, Read_port2_data_in, PC_Value : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         Alu_result_in, SP : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
         Write_Add_1_in, Write_Add_2_in : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
         Immediate_data_in, Write_Data2_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -62,14 +63,18 @@ ARCHITECTURE Memory_Stage_Deisgn OF Memory_Stage IS
     SIGNAL Address : STD_LOGIC_VECTOR (11 DOWNTO 0);
     SIGNAL Result_Mux4x1_level_1_Data : STD_LOGIC_VECTOR (31 DOWNTO 0);
     SIGNAL Result_Mux4x1_level_1_Address : STD_LOGIC_VECTOR (31 DOWNTO 0);
+    SIGNAL CCR_Write_Data : STD_LOGIC_VECTOR (31 DOWNTO 0);
     SIGNAL Result_Mux4x1_level_2 : STD_LOGIC_VECTOR (31 DOWNTO 0);
     SIGNAL OR_OUT : STD_LOGIC_VECTOR (11 DOWNTO 0);
     SIGNAL Result_Mux2x1 : STD_LOGIC_VECTOR (11 DOWNTO 0);
     SIGNAL Write_Mem_enable : STD_LOGIC;
+
 BEGIN
+CCR_Write_Data(3 downto 0)<=CCR_Out;
+CCR_Write_Data(31 downto 4)<=x"0000000"
 
     Mux4x1__level_1_Data : Mux4x1 GENERIC MAP(32) PORT MAP(Read_port2_data_in, PC_Value, x"00000000", x"00000000", Write_Data_Selector, Result_Mux4x1_level_1_Data);
-    Mux4x1__level_2 : Mux4x1 GENERIC MAP(32) PORT MAP(Result_Mux4x1_level_1_Data, PC_Address_INT_OUT, CCR_Out, x"00000000", Mux_Select_INT_FSM, Result_Mux4x1_level_2);
+    Mux4x1__level_2 : Mux4x1 GENERIC MAP(32) PORT MAP(Result_Mux4x1_level_1_Data, PC_Address_INT_OUT, CCR_Write_Data, x"00000000", Mux_Select_INT_FSM, Result_Mux4x1_level_2);
     Mux4x1__level_1_Address : Mux4x1 GENERIC MAP(12) PORT MAP(Alu_result_in, Read_port2_data_in(11 DOWNTO 0), SP, x"00000000", Mem_Add_selector, Result_Mux4x1_level_1_Address);
     OR_OUT <= MEM_Add_MUX_RTI_Select OR MEM_Add_MUX_INT_Select;
     Mux2x1 : Mux2x1 GENERIC MAP(12) PORT MAP(Result_Mux4x1_level_1_Address, SP, OR_OUT, Result_Mux2x1);
